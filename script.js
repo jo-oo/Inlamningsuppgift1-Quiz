34313ce08cd883067809a1f2274994d70ef7be9b
Johanna <johannakoreo@gmail.com>
2021-11-28 21:17:58 +0100

    gjort en del, bland annat -funktion: samma bild visas inte i spelet mer än en gång -visar de namn du gissat rätt på i slutet som text -satt rubriker på de sista listorna när de visas i slutet -redigerat ut en del onödig kod -förbättrat kommenterar inför "relase"

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-// hämtat diven #image-container. sparar den html-diven i en variabel i Js som heter imageContainerEl.
+//get elements from DOM
 let mainDivEl = document.getElementById('main-div');
 let imageContainerEl = document.getElementById('image-container');
 let alternativesEl = document.getElementById('alternatives');
 let messageEl = document.getElementById('message');
 let scoreEl = document.getElementById('score');
+let resultsEl = document.getElementById('results');
+let correctResultsEl = document.getElementById('correctResults');
 
 
-//----SHUFFLE OBJECTS - GETS LIST OF RANDOM STUDENTS---
-//Skapar en kopia på students-array: döper till randomStudents.ger den värdet av students-arrayen. skapar ny array av bara studenterna.  visas som varje person inuti den ursprungliga arrayen. Borde jag ha MAP här som innan?
-const randomStudents = students;
-console.log(randomStudents); //visar lista med studenterna i random ordning, precis som nedan, raden lite under. Hm...
+//map new array with one added property named "hasOccured"
+const randomStudents = students.map(student => {
+    student.hasOccured=false; 
+    return student;
+});
 
-//----SHUFFLE-----Fisher-Yates algorith
+//Shuffle-function: Fisher-Yates algorith
 const shuffleArrayOfStudents = (array) => {
 	for (let i = array.length - 1; i > 0; i--) {
 		const j = Math.floor(Math.random() * (i + 1));
@@ -20,84 +23,69 @@ const shuffleArrayOfStudents = (array) => {
 		array[j] = temp;
 	}
 }
-shuffleArrayOfStudents(randomStudents);
+shuffleArrayOfStudents(randomStudents); //shuffles randomStudents array
 console.log("Shuffled student is: ", randomStudents[0]);//visar bara en student. en slumpad student
+randomStudents[0].hasOccured=true;
 //---------------------------------//
 console.log("shufflade studenter :", randomStudents);//visar hela "nya" arrayen med shufflade studenter
 
 //----SHUFFLE--4--OBJECTS/STUDENTS-ARRAY----
-//skapar ny array av 4 studenter(från index 0,1,2,3): sparar i "Alternatives". Visas bara 1a gången på sidan, innan click-eventet
+//skapar ny array av 4 studenter(från index 0,1,2,3). Visas bara 1a gången på sidan, innan click-eventet
 let alternatives = randomStudents.slice(0, 4);
 console.log(alternatives); //skriver ut 4 SHUFFLADE studenter i ordningen 0-4, dvs 0 är rätt och ligger först i knapparna
-shuffleArrayOfStudents(alternatives); //"Q". -shufflar 4 namnen / knapparna i hur de VISAR så de visas på olika platser första gången. (alltså att 0/rätt student imte är default på första knappen) 
+shuffleArrayOfStudents(alternatives); //-shufflar 4 namnen / knapparna i hur de VISAR så de visas på olika platser första gången. 
 
 
 //----REDIGERAR INNER_HTML för ALRTERNATIVES (4 BUTTONS)----
-//Loppar över min alternative-array och sätter den till att för varje loop skapa en knapp med varje alternatives/students namn på. Kommer skapa 4 knappar pga alternative-arrayen är 4 students/elements lång
-
-//för att slippa kopiera detta och lägga det i min click-funtion så gör jag en funktion av det:
+//Loppar över min alternative-array med 4 studenter: för varje loop skapas en knapp med varje alternatives/students namn på. = 4 knappar skapas
 function displayButtons(alt) {
-    alternativesEl.innerHTML=""; //sätter den till att vara tom innan loopen börjar. Sen läggs innehåll till 4 gånger och skapar på så sätt 4 knappar
+    alternativesEl.innerHTML=""; //empty before loop
     alt.forEach(element => {
         alternativesEl.innerHTML += `
         <button name="${element.name}">${element.name}</button>
         `
     });
   }
+  displayButtons(alternatives); //visar 4 knappar i random ordning
+
+//------REDIGERAR INNER-HTML för IMAGES--------//
+imageContainerEl.innerHTML = `
+<img src='${randomStudents[0].image}' class="img-fluid"></img>	
+`//shows random image currently at index 0 from randomStudents-array
 
 
-function displayRightAnswers(alter) {
-alter.forEach(element => {
-                    alternativesEl.innerHTML += `
-                    <img src='${element.image}' class="img-fluid"></img> <p name="${element.name}">${element.name}</p>
-                    `
-                   
-                
-            });
-        }
-  
-  displayButtons(alternatives); //visar 4 knappar i den random ordning jag skapade ovan i "Q"
-
-  function displayStartButton() {
-    alternativesEl.innerHTML = `
-    <button type="Start">Play again</button>
-  `
-}
-  displayButtons(alternatives); //visar 4 knappar i den random ordning jag skapade ovan i "Q"
-
-//------------------------------------------------
-//ANTAL RÄTT-RÄKNARE
-
-let correctAnswers = 0;
-let wrongAnswers = 0;
-let totalScore = 0;
-let highScore = 0;
-let continueGame = true;
-//------------------------------------------------
-//HÄNDER NÄR SIDAN LADDAS om
-//FUNKTION:
+//creates new question with new image and adds 4 new buttons through the displayButtons-function
 const newQuestion = () =>{
-    //SKAPAR NY BILD+KNAPPAR
-    shuffleArrayOfStudents(randomStudents);//XY shufflar students objects
+    shuffleArrayOfStudents(randomStudents);//shuffles students
+    
+    while(randomStudents[0].hasOccured){
+        shuffleArrayOfStudents(randomStudents);
+    }
+    
 
-    //-->sätter i image-containern ny bild 0
     imageContainerEl.innerHTML = `
     <img src='${randomStudents[0].image}' class="img-fluid"></img>	
     `
-
-    //XY uppdaterar alternatives-knapparna efter att de shufflats på nytt ovan så de inte är samma som första eller förra gången. nu kommer 0 i knapp/namn-arrayen att vara samma som 0 i images-arrayen ovan på XY-raden
+    randomStudents[0].hasOccured=true;
+    console.log(randomStudents);
+    //creates list of 4 alternatives to equal randomStudent att index 0-3. Button [0] is same as image [0] and so on
     alternatives = randomStudents.slice(0, 4);
-
-    //nu vill jag också SHUFFLA KNAPPARNAS PLACERINGSORDNING
-    shuffleArrayOfStudents(alternatives); //shufflar 4 namnen-knapparna i hur de VISAR så de visas på olika platser första gången.
-
-    //visar knapparna med funktionen som lägger in 4 nya namn
+    //shuffle the alternatives-array so the placement is random
+    shuffleArrayOfStudents(alternatives);
+    //calls the function that creates 4 new buttons
     displayButtons(alternatives);
 }
 
-//HIGHSCORE - FUNCTION
-//lista för att lagra highscores
 
+//-----COUNTER -----
+let correctAnswers = 0; //amount of correct answers user has
+let totalScore = 0; //max amount of questions
+let highScore = 0; //highscore is 0 at first
+let continueGame = true;
+//------------------------------------------------
+
+
+//Highscore-function
 const newHighscore = () =>{
     //new high score?
       if (correctAnswers > highScore) {
@@ -111,73 +99,77 @@ const newHighscore = () =>{
       highScore = totalScore;
   }*/
 
-//--------------------LISTA MED RÄTT GISSADE STUDENTER------
-let wrongGuesses = []
-let rightGuesses = []
-let rightAndWrongGuesses = []
 
-/*
-const rightAndWrongGuesses1 = rightGuesses.map(rightGuess => {
-    return rightGuess
-})
-console.log(rightAndWrongGuesses1);
-*/
+//List showed att end with users results
+let results = [] 
 
+function displayResult(results) {
+    //filters not corrct answers
+    let notCorrectAnswers = results.filter(result => !result.correctAnswers );
 
+    let correctAnswers = results.filter(result => result.correctAnswers);
+    //Show wrong results with image and name
+        if (notCorrectAnswers.length != 0){
+            resultsEl.innerHTML += '<h2>You need to practice these:</h2><br/>'
+        }
+   
+            notCorrectAnswers.forEach(element => {
+                resultsEl.innerHTML += `
+                <img src='${element.image}' class="img-fluid"></img> <p> You guessed : ${element.youGuessed} <br/> Correct answer was: ${element.correct}</p>
+               `
+            });
 
+        //shows right result in text
+    if (correctAnswers.length != 0){
+        correctResultsEl.innerHTML += '<h2>You know these ones:</h2><br/>'
+    }
+        correctAnswers.forEach(element => {
+            correctResultsEl.innerHTML += `
+            <p>${element.youGuessed}</p>
+            `
+        });
+}   
 
-
-
-
-
-
-
-
-
-//------REDIGERAR INNER-HTML för IMAGES--------//
-imageContainerEl.innerHTML = `
-<img src='${randomStudents[0].image}' class="img-fluid"></img>	
-`//visar den slumpade bilden currently at index 0 from images-array
-
-
-//EVENTLISTENER: "lyssnar" efter att anv. klickar på hemsidan
-    addEventListener('click', e => {
-        //förhindrar att sidan laddas om (vilket är default-action för click-event)
-	    e.preventDefault();
-	
-        //kollar så anv. klickar på en knapp
-        //om så: hämta attributet name & spara i "clickedButton"
+//adds EventListener for click-events
+addEventListener('click', e => {
+    //prevents default page reload
+	e.preventDefault();
+        //if user clicked a button - get attribute "name" and save in "clickedButton"    
 	    if (e.target.tagName === "BUTTON") {
             let clickedButton = e.target.getAttribute('name');
-            //om attributet name (sparat i "clickedButton") är samma som random student namn= namnet är rätt & innehållet i diven som håller img ändras till att visa ny bild på random student image)
-      
-           if(e.target.getAttribute("type")=== "playagain"){
-               totalScore = 0;
-               messageEl.innerHTML = "";
-           }
-            if (clickedButton == randomStudents[0].name) { //är randomstudents [0]första gången
+           
+            //if user clicks "Play again-button"-> 
+           if(e.target.getAttribute("type") === "playagain"){
+                totalScore = 0; 
+                results.length = 0; // resets results-array.
+                //empties div´s
+                scoreEl.innerHTML = ""; 
+                messageEl.innerHTML = ""; 
+                correctResultsEl.innerHTML = "";
+                resultsEl.innerHTML = "";
+                //sets random students propertu "hasOccured" to be false again
+                const randomStudents = students.map(student => {
+                    student.hasOccured=false; 
+                    return student;
+                });
+            }
 
-                rightGuesses.push(randomStudents[0]);
-
-                console.log(rightGuesses);
-              /*  let selectedstudent = students.filter(student => student.name = randomStudents[0])
-                rightGuesses.push(selectedstudent)
-                */
+            //if clicked name equals RandomStudents[0] name, name is correct. I& innehållet i diven som håller img ändras till att visa ny bild på random student image)
+            if (clickedButton == randomStudents[0].name) { 
                 correctAnswers++;
                 totalScore++;
-               // totalScore = totalScore + correctAnswers;
-                console.log("Total score is: ", totalScore);
                 scoreEl.innerHTML = `
                 <p>Score: ${correctAnswers} / ${totalScore}</p>	
                 `
                 messageEl.innerHTML = `
                 <p>Right name! You guessed: ${clickedButton}</p>
                 `
+                let thisResult = {youGuessed: clickedButton, correct: randomStudents[0].name, image:randomStudents[0].image, correctAnswers: true}
+                results.push(thisResult);
+                console.log(results);
+            }   
             
-            }
-    
-            else if (clickedButton == randomStudents[1].name || clickedButton == randomStudents[2].name || clickedButton == randomStudents[3].name){
-                //UPPDTAERAD SCORE
+            else if (clickedButton == randomStudents[1].name ||       clickedButton == randomStudents[2].name || clickedButton == randomStudents[3].name){
                 totalScore++;
                // totalScore = totalScore + correctAnswers;
                 console.log("Total score is: ", totalScore);
@@ -188,129 +180,28 @@ imageContainerEl.innerHTML = `
                 messageEl.innerHTML = `
                 <p>Wrong name! <br/> You guessed: ${clickedButton}. <br/>  Right name was: ${randomStudents[0].name}  </p>
                 `      
-                wrongGuesses.push(clickedButton);
-                console.log(wrongGuesses);
+                let thisResult = {youGuessed: clickedButton, correct: randomStudents[0].name, image:randomStudents[0].image, correctAnswers: false}
+                results.push(thisResult);
+                console.log(results);
             }  
-            //SKAPAR NY BILD+KNAPPAR
-          
+            
+            //prevents new image and buttons to appear after last round is played
             if(totalScore <= 3){
                 newQuestion();
             }
          
-            //sätter spelet till att vara 3 rundor
+            //sets game to equal 3 rounds
             if(totalScore === 3){
-                newHighscore();
-                console.log("HIGHSCORE IS", highScore);
-                messageEl.innerHTML = `<p>You got ${correctAnswers} right out of ${totalScore} </br>Your HIGHSCORE is ${highScore}</p> <button type="playagain">Play again</button>`
-               
-/*
-                if (highScore > highScore) {
-                    messageEl.innerHTML = `<p>You got ${correctAnswers} right out of ${totalScore} </br>Your HIGHSCORE is ${highScore}</p> <button type="playagain">Play again</button>`("YAY new High SCORE! High score is now " + highScore);
-                } else {
-                    console.log("Sorry, no new highscore. Your current highscore is " + highScore);
-                }
-            }*/
+                newHighscore(); //checks if new Highscore
+                messageEl.innerHTML = `<p>You got ${correctAnswers} right out of ${totalScore} </br>Your HIGHSCORE is ${highScore}</p> 
+                <button type="playagain">Play again</button>
+                ` 
                 correctAnswers = 0;
-                wrongAnswers = 0;
                 imageContainerEl.innerHTML = "";
                 alternativesEl.innerHTML = "";
-
-
-                displayRightAnswers(rightGuesses);
-
-
+                displayResult(results);
             }
-        
-            
-            /*
-            if(totalScore <= 3) {
-                mainDivEl.innerHTML = `Not so good`
-            } else if (totalScore > 3 && totalScore <= 5) {
-                mainDivEl.innerHTML = `You did ok`
-            } else if (totalScore > 5 && totalScore <= 7) {
-                mainDivEl.innerHTML = `Good job!`
-            } else if (totalScore > 7 && totalScore <= 10) {
-                mainDivEl.innerHTML = `You did great!`
-            }
-            */
-        
-            }
-        else {
+        } else {
             alert("You can´t win if you don´t try");
-    } 
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-//-------New stuff SHUFFLE NAMES------
-//MAP. skapar ny array av bara namnen. visas som varje persons namn inuti den ursprungliga arrayen
-const names = students.map(student => {
-    return student.name;
-});
-console.log(names);//visar lista med bara namnen
-//----SHUFFLE-----Fisher-Yates algorith
-//Loopar genom arrayen: tar ett random item och byter det till ett annat 
-const shuffleArrayOfNames = (array) => {
-	for (let i = array.length - 1; i > 0; i--) {
-		const j = Math.floor(Math.random() * (i + 1));
-		const temp = array[i];
-		array[i] = array[j];
-		array[j] = temp;
-	}
-}
-shuffleArrayOfNames(names);
-    //console.log("Shuffled names: ", names.join("\n"));//visar hela "nya" arrayen med alla namm
-    //console.log("Shuffled name is: ", names[0]);//visar bara ett namn. slumpat namn.
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-//-----TEST FILTER OCH REDUCE-------
-//TEST: FILTER: filtrerar ut namn längre än 18 tecken (mellansla ej inkluderat) ur den arrayen
-const longNames = students.filter(student => {
-    return student.name.length >18;
-}).map(student => {
-    return student.name;
-});
-console.log(longNames); //visar namnen över 18 tecken som hela objekt. 
-//när vi lägger till MAP i funtionen visas istället bara namnen på dom över 8 tecken. 
-//TEST: REDUCE. reduce down the array to a singular value. Tex: lägga ihop längden av alla namn 
-//kommer köra igenom varje item i min array (varje person) och
-const totalLongNames = students.reduce((total, student) => {
-    return total + student.name.length;
-}, 0); //börjar räkna på 0
-///student=objektet student
-//total=respresenterar nuvarande värde när vi loppar genom varje person. placeholder för värdet som sen blir vårt slutgiltiga resultat.
-console.log(totalLongNames); //590
-*/
+        } 
+}); 
